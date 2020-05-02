be4453bd8621980509dffbdaf0657198aaf31c90
Shrey Somaiya <z5257343@unsw.edu.au>
2020-05-02 17:19:43 +1000

    Updates

---
+++ precache-manifest.00871c5e76c5efbe4bc4181a87a5ffa3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16d589f20a233ff7095de6bd63ed1462",
+    "url": "/yes_or_no/index.html"
+  },
+  {
+    "revision": "b78e722f46f3c7e6dd06",
+    "url": "/yes_or_no/static/css/main.8285bb76.chunk.css"
+  },
+  {
+    "revision": "118aaf01ee92367e0b26",
+    "url": "/yes_or_no/static/js/2.ab153efd.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/yes_or_no/static/js/2.ab153efd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b78e722f46f3c7e6dd06",
+    "url": "/yes_or_no/static/js/main.6fc4e4fd.chunk.js"
+  },
+  {
+    "revision": "c08c09071c8efa17770e",
+    "url": "/yes_or_no/static/js/runtime-main.110dddb6.js"
+  }
+]);
